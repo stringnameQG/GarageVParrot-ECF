eaa653ba5752079393bd407dbc43939964a10afc
Quentin GUILLOU <Kentguillou@hotmail.fr>
2024-02-05 21:59:29 +0100

    Ajout du css de la page Accueil, ajout du scipt javascript navAffiche

---
+++ GarageV.Parrot/public/js/formulaireAffichage.js
@@ -0,0 +1,19 @@
+// On attende le chargement complét de la page 
+window.onload = () => {    /*
+    // On crée une variable liée a l'élément .nav-icon
+    let navIcon = document.querySelector(".nav-icon");
+    // On crée une variable liée à l'élément nav
+    let nav = document.querySelector(".nav-list");
+    // On crée une variable liée a l'élément .nav-icon
+    let navIconClose = document.querySelector(".nav-list__close-icon");
+    // On détecte le clique sur l'icon nav
+    navIcon.addEventListener('click', () => {
+        // On met le display de l'élément nav en flex pour l'afficher
+        nav.style.display = "flex";
+    })
+    // On détecte le clique sur l'icon close
+    navIconClose.addEventListener('click', () => {
+        // On met le display de l'élément nav en none pour le cacher
+        nav.style.display = "none";
+    })     */
+}
--- GarageV.Parrot/src/Controller/HomeController.php
+++ GarageV.Parrot/src/Controller/HomeController.php
@@ -1,18 +1,22 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\ServeRepository;
+use App\Repository\ViewRepository;
+use App\Repository\ScheduleRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-  #[Route('/', name: 'app_accueil')]
-  public function Accueil() : Response
+  #[Route('/', name: 'app_accueil', methods: ['GET'])]
+  public function Accueil(ServeRepository $serveRepository, ViewRepository $viewRepository, ScheduleRepository $scheduleRepository) : Response
   {
-    $nomPage = "Accueil";
     return $this->render('accueil/Accueil.html.twig', [
-      'nomPage' => $nomPage,
+      'serves' => $serveRepository->findAll(),
+      'views' => $viewRepository->findAll(),
+      'schedules' => $scheduleRepository->findAll(),
     ]);
   }
 }
